15424ac6ac2294a5b940fc40195ce98191b0fa11
Joao Dumba <72528227+tioya2923@users.noreply.github.com>
2026-01-03 07:07:16 +0000

    SearchProcurar Yes 12

--- src/api/search.js
+++ src/api/search.js
@@ -2,7 +2,7 @@ import api from "../api";
 
 export async function searchContent(query) {
   try {
-    const response = await api.get("/search", { params: { q: query } });
+    const response = await api.get("/api/search", { params: { q: query } });
     return response.data;
   } catch (error) {
     throw error;
