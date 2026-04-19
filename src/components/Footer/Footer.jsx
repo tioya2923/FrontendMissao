bd6b5a5f3b7130d565c9e36f8a48c3fab52de889
Joao Dumba <72528227+tioya2923@users.noreply.github.com>
2026-04-19 08:59:31 +0100

    remover o junta-se à nós

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,39 +1,9 @@
-import React from "react";
 import "./Footer.css";
-import "@fortawesome/fontawesome-free/css/all.min.css"; // Importação necessária
 
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <div className="footer-links">
-          <div className="title-footer">Junte-se a nós</div>
-
-          <div className="social-icons">
-            <a href="https://www.facebook.com/missaonohuambo/" target="_blank" rel="noopener noreferrer">
-              <i className="fa-brands fa-facebook-f"></i>
-            </a>
-            <a href="https://zeno.fm/radio/radiomissio-ide-e-anunciai" target="_blank" rel="noopener noreferrer">
-              <i className="fa-solid fa-tower-broadcast"></i> {/* Corrigido */}
-            </a>
-            <a href="https://www.instagram.com/missaonohuambo/" target="_blank" rel="noopener noreferrer">
-              <i className="fa-brands fa-instagram"></i>
-            </a>
-            <a href="https://www.youtube.com/@missaonohuambo5266" target="_blank" rel="noopener noreferrer">
-              <i className="fa-brands fa-youtube"></i>
-            </a>
-            <a href="https://www.tiktok.com/@missaonohuambo" target="_blank" rel="noopener noreferrer">
-              <i className="fa-brands fa-tiktok"></i>
-            </a>
-            <a href="https://www.flickr.com/photos/fotosdamissaonohuambo/with/52264207612" target="_blank" rel="noopener noreferrer">
-              <i className="fa-brands fa-flickr"></i>
-            </a>
-            <a href="https://www.linkedin.com/in/miss%C3%A3o-no-huambo-135ba1247/" target="_blank" rel="noopener noreferrer">
-              <i className="fa-brands fa-linkedin-in"></i>
-            </a>
-          </div>
-        </div>
-
         <span>© {new Date().getFullYear()} Missão. Todos os direitos reservados.</span>
       </div>
     </footer>
